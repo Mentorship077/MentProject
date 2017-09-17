2c9226a821d38034328a27a393a7ea3d4cc9aed6
Your Name <you@example.com>
2017-09-17 23:30:09 +0300

    minor

--- src/main/java/com/TALab4/gmailSeleniumTask/webdriverutils/WebDriverSingleton.java
+++ src/main/java/com/TALab4/gmailSeleniumTask/webdriverutils/WebDriverSingleton.java
@@ -1,5 +1,6 @@
 package com.TALab4.gmailSeleniumTask.webdriverutils;
 
+import com.TALab4.gmailSeleniumTask.util.EnvProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +11,7 @@ import java.util.concurrent.TimeUnit;
  * Created  on 17.09.17.
  */
 public class WebDriverSingleton {
-
+    private final static EnvProperties prop = EnvProperties.getInstance();
     private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
 
     private WebDriverSingleton() {
@@ -20,7 +21,7 @@ public class WebDriverSingleton {
         if (webDriver.get() != null) {
             return webDriver.get();
         }
-        //  System.setProperty("webdriver.chrome.driver", prop.getChomeDriverPath());
+//        System.setProperty("webdriver.chrome.driver", prop.getChomeDriverPath());
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 
@@ -31,6 +32,7 @@ public class WebDriverSingleton {
     public static void gotoURL(String url) {
         getInstance().navigate().to(url);
     }
+
     public static void clickFirstLink(String XPath) {
         webDriver.get().findElement(By.xpath(XPath)).click();
     }
