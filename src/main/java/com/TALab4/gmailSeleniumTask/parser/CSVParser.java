bc7311d41726c2e32513eb8c3f4d709e2938fe7a
Nazar_Khimin <Nazar_Khimin@epam.com>
2017-09-27 11:47:46 +0300

    was added csv parser

--- src/main/java/com/TALab4/gmailSeleniumTask/parser/CSVParser.java
+++ src/main/java/com/TALab4/gmailSeleniumTask/parser/CSVParser.java
@@ -10,26 +10,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVParser {
-    public static void main(String[] args) throws IOException {
+    private static final String USERS_XML_PATH = "src/test/resources/users.csv";
+    private static final String MESSAGES_XML_PATH = "src/test/resources/messages.csv";
 
-        csvUserParser();
-    }
-
-    private static Object[] csvUserParser() throws IOException {
-        CSVReader reader = new CSVReader(new FileReader("src/test/resources/users.csv"), ',');
-        List<User> emps = new ArrayList<>();
+    public static List<User> csvUserParser() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(USERS_XML_PATH), ',');
+        List<User> users = new ArrayList<>();
         String[] record = null;
         while ((record = reader.readNext()) != null) {
             User emp = new User();
             emp.setEmail(record[0]);
             emp.setPassword(record[1]);
-            emps.add(emp);
+            users.add(emp);
         }
         reader.close();
-        return emps.toArray();
+        return users;
     }
-    private static Object[] csvmESSAGEParser() throws IOException {
-        CSVReader reader = new CSVReader(new FileReader("src/test/resources/messages.csv"), ',');
+
+    public static List<Message> csvMessageParser() throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(MESSAGES_XML_PATH), ',');
         List<Message> messages = new ArrayList<>();
         String[] record = null;
         while ((record = reader.readNext()) != null) {
@@ -40,6 +39,6 @@ public class CSVParser {
             messages.add(message);
         }
         reader.close();
-        return messages.toArray();
+        return messages;
     }
 }
--- src/main/java/com/TALab4/gmailSeleniumTask/webdriverutils/DataProviderSource.java
+++ src/main/java/com/TALab4/gmailSeleniumTask/webdriverutils/DataProviderSource.java
@@ -1,5 +1,6 @@
 package com.TALab4.gmailSeleniumTask.webdriverutils;
 
+import com.TALab4.gmailSeleniumTask.parser.CSVParser;
 import com.TALab4.gmailSeleniumTask.parser.model.Message;
 import com.TALab4.gmailSeleniumTask.parser.model.User;
 import com.opencsv.CSVReader;
@@ -19,7 +20,7 @@ public class DataProviderSource {
     public static Object[][] getUsersAndMessages() throws JAXBException, IOException {
 //        List<User> usersList = XMLParser.parseUsers();
 //        List<Message> messageList = XMLParser.parseMessages();
-        return generateDemensionArray(csvUserParser(), csvMessageParser());
+        return generateDemensionArray(CSVParser.csvUserParser(), CSVParser.csvMessageParser());
     }
 
     private static Object[][] generateDemensionArray(List userList, List messages) {
@@ -31,33 +32,7 @@ public class DataProviderSource {
         }
         return objArray;
     }
-    private static List<User> csvUserParser() throws IOException {
-        CSVReader reader = new CSVReader(new FileReader("src/test/resources/users.csv"), ',');
-        List<User> emps = new ArrayList<>();
-        String[] record = null;
-        while ((record = reader.readNext()) != null) {
-            User emp = new User();
-            emp.setEmail(record[0]);
-            emp.setPassword(record[1]);
-            emps.add(emp);
-        }
-        reader.close();
-        return emps;
-    }
-    private static List<Message> csvMessageParser() throws IOException {
-        CSVReader reader = new CSVReader(new FileReader("src/test/resources/messages.csv"), ',');
-        List<Message> messages = new ArrayList<>();
-        String[] record = null;
-        while ((record = reader.readNext()) != null) {
-            Message message = new Message();
-            message.setTo(record[0]);
-            message.setSubject(record[1]);
-            message.setTextMessage(record[2]);
-            messages.add(message);
-        }
-        reader.close();
-        return messages;
-    }
+
 
    /* @Test(dataProvider = "usersAndMessages")
     public void gmailTest(User userList,Message messageList) throws JAXBException, InterruptedException {
