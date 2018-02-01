5c433065dad7192ad94ffdd6c351a5608b8786ab
Nazar_Khimin <Nazar_Khimin@epam.com>
2018-02-01 18:09:19 +0200

    cucumber add some workpieces

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^a calculator I just turned on$")
+    public void aCalculatorIJustTurnedOn() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I add (\\d+) and (\\d+)$")
+    public void iAddAnd(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
